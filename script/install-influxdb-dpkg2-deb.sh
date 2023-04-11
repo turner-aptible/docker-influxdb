01bf469582e774df1c29deb8c0925a6afae30a35
John Turner <john.turner@aptible.com>
2023-04-11 12:12:41 -0400

    Adding in 2.0 deps to build

--- script/install-influxdb-dpkg2-deb.sh
+++ script/install-influxdb-dpkg2-deb.sh
@@ -3,6 +3,12 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+BUILD_DEPS=(curl)
+
+apt-get update
+apt-get -y install "${BUILD_DEPS[@]}"
+rm -rf /var/lib/apt/lists/*
+
 FILE="influxdb2-${INFLUXDB_VERSION}-amd64.deb"
 URL="https://dl.influxdata.com/influxdb/releases/${FILE}"
 
